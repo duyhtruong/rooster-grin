1905daebfe803c0471310a0e4475efb115d83f02
Duy Truong <duytruongh@gmail.com>
2019-07-25 22:44:11 -0700

    body

--- src/components/BodyOne.js
+++ src/components/BodyOne.js
@@ -1,10 +1,31 @@
 import React from 'react';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
+import imgClimb from '../assets/img-1.jpg';
 
 class BodyOne extends React.Component{
+
+
+    renderColumns =()=>{
+        return(
+                <Container fluid={true}>
+                <Row noGutters={true}>
+                    <Col sm className='bodyHeight-75vh'> <p>stuff</p> </Col>
+
+                    <Col sm className='bodyHeight-75vh'><img className='imgWidth-100' src={imgClimb} alt='Climb Image' /></Col>
+                </Row>
+                </Container>
+        
+            
+        )
+    }
+
     render(){
         return(
             <div className='bodyHeight-75vh'>
-                BodyOne
+                  {this.renderColumns()}        
             </div>
         )
     }
