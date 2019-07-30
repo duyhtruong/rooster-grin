f1fd18db379b96123bf3dd8d65e30950a24537ed
Duy Truong <duytruongh@gmail.com>
2019-07-29 21:53:15 -0700

    Updates

---
+++ precache-manifest.6ff7a56411021894f02cd842bea6c3ff.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "121c0120bfed7a27076ae1e945599625",
+    "url": "/rooster-grin/index.html"
+  },
+  {
+    "revision": "0f1fb876abcaf2e48a4d",
+    "url": "/rooster-grin/static/css/main.f750f69b.chunk.css"
+  },
+  {
+    "revision": "103fc715252a2151931d",
+    "url": "/rooster-grin/static/js/2.e2584e52.chunk.js"
+  },
+  {
+    "revision": "0f1fb876abcaf2e48a4d",
+    "url": "/rooster-grin/static/js/main.5a9b41ab.chunk.js"
+  },
+  {
+    "revision": "fa1a943ebf280ed79813",
+    "url": "/rooster-grin/static/js/runtime~main.ad0518f9.js"
+  },
+  {
+    "revision": "d679e95e3e1a4102ea2a1ef27bd34899",
+    "url": "/rooster-grin/static/media/banner-1.d679e95e.jpg"
+  },
+  {
+    "revision": "541a7d9ecf6e06e41906b9424af075ac",
+    "url": "/rooster-grin/static/media/banner-2.541a7d9e.jpg"
+  },
+  {
+    "revision": "7bc311e4e86ac578b19c3e27c1d2aa01",
+    "url": "/rooster-grin/static/media/facebook.7bc311e4.svg"
+  },
+  {
+    "revision": "e2af670d307bfa94a7ee5ce2408dc000",
+    "url": "/rooster-grin/static/media/hero.e2af670d.jpg"
+  },
+  {
+    "revision": "4631d634d77566ea790be3171fd1adea",
+    "url": "/rooster-grin/static/media/home-expertise.4631d634.svg"
+  },
+  {
+    "revision": "2647fa6b7f23e3a31808e475df20b241",
+    "url": "/rooster-grin/static/media/home-hygiene.2647fa6b.svg"
+  },
+  {
+    "revision": "c97ae485885baadfc689ca82304bcfc7",
+    "url": "/rooster-grin/static/media/home-lab.c97ae485.svg"
+  },
+  {
+    "revision": "79cb05a3c9e1bf2f1c335e1acfa02296",
+    "url": "/rooster-grin/static/media/home-retention.79cb05a3.svg"
+  },
+  {
+    "revision": "cefc803512d923ce6d6d3f6f6cd7feb9",
+    "url": "/rooster-grin/static/media/img-1.cefc8035.jpg"
+  },
+  {
+    "revision": "417149bccf5f7ef142c1e2aa48778220",
+    "url": "/rooster-grin/static/media/instagram.417149bc.svg"
+  },
+  {
+    "revision": "0d146ca16f8a3eebe272a80c2d4f695e",
+    "url": "/rooster-grin/static/media/logo-white.0d146ca1.svg"
+  }
+]);
